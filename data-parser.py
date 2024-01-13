d0b73ecfd773157eec3bbe1708c5fe3103c88b6b
endothermiic <christina.pizzonia@mail.utoronto.ca>
2024-01-13 12:25:13 -0500

    new file:   clean-data.cpp 	modified:   data-parser.py 	modified:   select-menu.ino 	new file:   timing.ino

--- data-parser.py
+++ data-parser.py
@@ -1,34 +1,37 @@
 import json
 
-schedules = [
-    "Tuesday 1",
-    "Tuesday 2",
-    "Wednesday 1",
-    "Wednesday 2",
-    "Thursday 1",
-    "Thursday 2",
-    "Friday 1",
-    "Friday 2"
-]
+schedules = []
+first_entry = 1
+
+first_day = "monday"  # Should reset after every year - change the start date
 
 with open("pickup-data-orig.json", 'r') as file:
     data = json.load(file)
 
-# # Open a new C++ file for writing
-# with open('clean-data.cpp', 'w') as output_file:
-#     # C++ code to print waste types based on schedule
-#     output_file.write('#include <iostream>\n\n')
-#     output_file.write('int main() {\n\t')
-#     #parseFile(file)
-#     output_file.write('\treturn 0;\n}')
+# Open a new C++ file for writing
+with open('clean-data.cpp', 'w') as output_file:
+    output_file.write('#include <iostream>\n\n')
+    output_file.write('int main() {\n\t')
+    # loop over input data and initialize arrays
+    for entry in data:
+        schedule = entry.get("Schedule")
+        if schedule in schedules:
+            output_file.write(', ')
+        if schedule not in schedules:
+            if not first_entry:
+                output_file.write('};\n\t')
+            output_file.write(
+                'int ' + schedule.replace(" ", "") + '[] = {')
+            schedules.append(schedule)
+            first_entry = 0
 
+        organics = 0 if entry.get("Organics") == '0' else 1
+        garbage = 0 if entry.get("Garbage") == '0' else 1
+        recycling = 0 if entry.get("Recycling") == '0' else 1
+        yard_waste = 0 if entry.get("YardWaste") == '0' else 1
 
-for entry in data:
-    collection_date = entry.get('CollectionDate')
-    print(collection_date)
-    # # Assuming '1' indicates pickup, '0' indicates no pickup
-    # pickup_status = 1 if entry.get('Pickup') == 'Yes' else 0
+        output_file.write(str(organics*1000 + garbage*100 +
+                          recycling*10 + yard_waste))
 
-    # # Extract week number from the date and update the array
-    # week_number = int(collection_date.split('-')[1])
-    # pickup_array[week_number - 1] = pickup_status
+    output_file.write('};\nreturn 0;\n}')
+    output_file.close()
